14a0f563295d7cd34ff411b7bd3224f795696c53
villalbaweb <villalbaweb@gmail.com>
2018-05-21 21:10:34 -0500

    Adding PagesModule to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// routes
+import { APP_ROUTES } from './app.routes';
+
+// modules
+import { PagesModule } from './pages/pages.module';
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graphs1Component } from './pages/graphs1/graphs1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graphs1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
+    PagesModule,
     APP_ROUTES
   ],
   providers: [],
---
+++ src/app/pages/pages.routes.ts
@@ -0,0 +1,21 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graphs1Component } from './graphs1/graphs1.component';
+import { PagesComponent } from './pages.component';
+
+const pagesRoutes: Routes = [
+    {
+        path: '',
+        component: PagesComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent },
+            { path: 'progress', component: ProgressComponent },
+            { path: 'graphs1', component: Graphs1Component },
+            { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+        ]
+    }
+];
+
+export const PAGES_ROUTES = RouterModule.forChild(pagesRoutes);
