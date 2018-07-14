d6830dbf08fba440b26f97aaeeea33c25afbe072
villalbaweb <villalbaweb@gmail.com>
2018-07-14 15:38:53 -0500

    Hacer sidebar dinamico basado en la data de sidebar service

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -3,5 +3,16 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SidebarService {
 
+    menu: any = [
+      {
+        titulo: 'Principal',
+        icono: 'mdi mdi-gauge',
+        submenu: [
+          { titulo: 'Dashboard', url: '/dashboard'},
+          { titulo: 'Progress Bar', url: '/progress'},
+          { titulo: 'Graficas', url: '/graphs1'},
+        ]
+      }
+    ];
   constructor() { }
 }
