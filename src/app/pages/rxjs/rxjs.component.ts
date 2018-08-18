cdc5c0f7aa25b649fe70a8c33da8c4f520982b03
villalbaweb <villalbaweb@gmail.com>
2018-08-18 10:47:50 -0500

    Adding a basic observable object with the pipe operator

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { retry } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -7,7 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RxjsComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+
+    let obs = new Observable( observer => {
+      let counter = 0;
+      let intervalo = setInterval(() => {
+        counter++;
+        observer.next(counter);
+        if(counter === 3) {
+          observer.complete();
+          clearInterval(intervalo);
+        } else if(counter === 2) {
+          clearInterval(intervalo);
+          observer.error('El contador alcanzo 2...');
+        }
+      }, 1000);
+    });
+
+    obs
+    .pipe(
+      retry(2)
+    )
+    .subscribe(
+      (numero) => {
+        console.log(numero);
+      },
+      (error) => {console.log(error);},
+      () => { console.log('El observer completed...');
+    });
+  }
 
   ngOnInit() {
   }
