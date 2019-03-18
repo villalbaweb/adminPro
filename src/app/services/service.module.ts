d83a6bf67fe462ddd720d46abf39bc37c465e3c5
villalbaweb <villalbaweb@gmail.com>
2019-03-18 12:33:02 -0600

    Adding LoginGuard

--- src/app/services/service.module.ts
+++ src/app/services/service.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
-import { SettingsService, SharedService, SidebarService, UsuarioService } from './service.index';
+import { SettingsService, SharedService, SidebarService, UsuarioService, LoginGuard } from './service.index';
 
 @NgModule({
   imports: [
@@ -13,7 +13,8 @@ import { SettingsService, SharedService, SidebarService, UsuarioService } from '
     SettingsService,
     SharedService,
     SidebarService,
-    UsuarioService
+    UsuarioService,
+    LoginGuard
   ]
 })
 export class ServiceModule { }
