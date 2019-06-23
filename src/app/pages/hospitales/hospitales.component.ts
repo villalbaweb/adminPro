c98f95950d06835fedd531fb4c26abd704d78373
villalbaweb <villalbaweb@gmail.com>
2019-06-23 15:00:38 -0500

    Agregando metodo CargarHospitales a HospitalService

--- src/app/pages/hospitales/hospitales.component.ts
+++ src/app/pages/hospitales/hospitales.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HospitalService } from 'src/app/services/service.index';
+import { Hospital } from 'src/app/models/hospital.model';
+
 @Component({
   selector: 'app-hospitales',
   templateUrl: './hospitales.component.html',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HospitalesComponent implements OnInit {
 
-  constructor() { }
+  hospitales: Hospital[] = [];
+
+  constructor(private hospitalService: HospitalService) { }
 
   ngOnInit() {
+    this.hospitalService.CargarHospitales(0, 5)
+    .subscribe((hospitals: any) => {
+      this.hospitales = hospitals.hospitales;
+      console.log(this.hospitales);
+    });
   }
 
 }
--- src/app/services/hospital/hospital.service.ts
+++ src/app/services/hospital/hospital.service.ts
@@ -1,7 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+import swal from 'sweetalert';
+
+import { Usuario } from 'src/app/models/usuario.model';
+import { URL_SERVICIOS } from 'src/app/config/config';
 
 @Injectable()
 export class HospitalService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  CargarHospitales( skip: number, take: number ) {
+    let url = URL_SERVICIOS + `/hospital?skip=${skip}&take=${take}`;
+
+    return this.http.get( url );
+  }
 }
